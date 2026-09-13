1ddc8e5422b46c818343e934cd8cd643d0a17c28
Rolando HR <rolando167@hotmail.com>
2026-09-13 12:28:01 +0200

    domingo

--- multimodulo-capas/mapper/target/generated-sources/annotations/com/curso/mapper/CursoMapperImpl.java
+++ multimodulo-capas/mapper/target/generated-sources/annotations/com/curso/mapper/CursoMapperImpl.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-09-13T11:29:59+0200",
-    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.46.0.v20260528-0407, environment: Java 25.0.3 (Eclipse Adoptium)"
+    date = "2026-09-13T12:12:31+0200",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.10 (Oracle Corporation)"
 )
 @Component
 public class CursoMapperImpl implements CursoMapper {
