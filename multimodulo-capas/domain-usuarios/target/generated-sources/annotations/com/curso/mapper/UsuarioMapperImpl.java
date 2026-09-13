00ce9bc244a2c40a3f2af9ef42777995552749fc
Rolando HR <rolando167@hotmail.com>
2026-09-13 12:30:16 +0200

    domingo

--- multimodulo-capas/domain-usuarios/target/generated-sources/annotations/com/curso/mapper/UsuarioMapperImpl.java
+++ multimodulo-capas/domain-usuarios/target/generated-sources/annotations/com/curso/mapper/UsuarioMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-09-13T12:12:35+0200",
+    date = "2026-09-13T12:28:32+0200",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.10 (Oracle Corporation)"
 )
 @Component
--- multimodulo-capas/mapper/target/generated-sources/annotations/com/curso/mapper/CursoMapperImpl.java
+++ multimodulo-capas/mapper/target/generated-sources/annotations/com/curso/mapper/CursoMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-09-13T12:12:31+0200",
+    date = "2026-09-13T12:28:29+0200",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.10 (Oracle Corporation)"
 )
 @Component
